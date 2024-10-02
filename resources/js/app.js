eac3a18b754584eb78f99d990ff7f3823f501aaa
fuad2101 <fuady.teknikinformatika@gmail.com>
2024-10-02 15:26:24 +0800

    feat: landing,register and login page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 import "./bootstrap";
-import "./oxhen-template/aos.js";
-import "./oxhen-template/app.js";
-import "./oxhen-template/bootstrap.bundle.min.js";
-import "./oxhen-template/contact.js";
-import "./oxhen-template/counter.js";
-import "./oxhen-template/particles.min.js";
-import "./oxhen-template/swiper-bundle.min.js";
+import "./oxhen-template/bootstrap.bundle.min";
+import "./oxhen-template/counter";
+import "./oxhen-template/aos";
+import "./oxhen-template/swiper-bundle.min";
+import "./oxhen-template/contact";
+import "./oxhen-template/particles.min";
+import "./oxhen-template/app";
 
 import Alpine from "alpinejs";
 

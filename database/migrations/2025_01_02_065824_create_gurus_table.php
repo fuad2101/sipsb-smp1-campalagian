bff3b4fda475e72d120d35931015f9092dc73e43
fuad2101 <fuady.teknikinformatika@gmail.com>
2025-01-02 15:22:47 +0800

    Export gurus SQL

--- database/migrations/2025_01_02_065824_create_gurus_table.php
+++ database/migrations/2025_01_02_065824_create_gurus_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('gurus', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('mata_pelajaran')->nullable();
+            $table->string('jabatan')->nullable();
             $table->timestamps();
         });
     }

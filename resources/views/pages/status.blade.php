fd25bc63b8c544dc42a23dc68fffefd4dfc58a61
fuad2101 <fuady.teknikinformatika@gmail.com>
2025-01-08 14:42:02 +0800

    add status bayar migration, add status input for upload bukti bayar

--- resources/views/pages/status.blade.php
+++ resources/views/pages/status.blade.php
@@ -4,14 +4,20 @@
     <h2>Status Pendaftaran:
         @foreach ($foo as $val)
             @if ($val['status_daftar'] == 'verifikasi')
-        <span class="badge bg-warning">Dalam proses Verifikasi</span> </h2>
-        @else
-        <span class="badge bg-success">Lulus Berkas</span> </h2>
-        Silahkan download pendaftaran
-        <form action="/download">
-            <button class="btn btn-lg btn-primary">Download Kartu Daftar </button>
-        </form>
-        @endif
+            <span class="badge bg-warning">Dalam proses Verifikasi</span> </h2>
+            @else
+            <span class="badge bg-success">Lulus Berkas</span> </h2>
+            <form action="/download">
+                <label for="">Upload Bukti Pembayaran</label><input class="form-control form-control-sm @error('bukti_bayar') is-invalid @enderror"" type="file" name="bukti_bayar" id="" accept="image/*">
+                @error('bukti bayar')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
+            </div>
+            </form>
+            <a class="btn btn-primary my-4" href="">Download Kartu Pendaftaran</a>
+            @endif
         @endforeach
 
 @endsection

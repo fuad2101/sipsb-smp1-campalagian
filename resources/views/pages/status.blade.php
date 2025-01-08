51f8acc49672630024429ae78bc9a15a5666c636
fuad2101 <fuady.teknikinformatika@gmail.com>
2025-01-08 14:22:27 +0800

    typo 'verfikasi'

--- resources/views/pages/status.blade.php
+++ resources/views/pages/status.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h2>Status Pendaftaran:
         @foreach ($foo as $val)
-            @if ('verfikasi')
+            @if ($val['status_daftar'] == 'verifikasi')
         <span class="badge bg-warning">Dalam proses Verifikasi</span> </h2>
         @else
         <span class="badge bg-success">Lulus Berkas</span> </h2>
